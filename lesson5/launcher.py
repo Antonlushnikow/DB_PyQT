3a6a34c6c1a6a8b0009857a9086f26320ade6d1a
Anton Lushnikov <antonlushnikow@gmail.com>
2021-12-25 17:13:44 +0500

    lesson5.41

--- lesson5/launcher.py
+++ lesson5/launcher.py
@@ -14,7 +14,7 @@ def main():
     except IndexError:
         print('Не указан аргумент. Запускается два клиента')
     finally:
-        # processes.append(subprocess.Popen('python ./server/app/main.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
+        processes.append(subprocess.Popen('python ./server/app/main.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
         for _ in range(count):
             processes.append(subprocess.Popen('python ./client/app/main.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
 
